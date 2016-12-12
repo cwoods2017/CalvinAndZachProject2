c0a8bfbdd5bbd0560acd1e94de57931f3e570ae3
Calvin Woods <cwoods2017@francisparker.org>
2016-12-12 15:41:56 -0800

    Starting to Work on Orbit Stuff
    starting to work on orbit stuff

--- Gravity.js
+++ Gravity.js
@@ -1,15 +1,30 @@
 
-var inc = -.1;
+var xspeed = 0;
+var yspeed = 0;
+var xpos = 500;
+var ypos = 30;
+var yacceleration = .2;
+var xacceleration = 0;
+var negativeYAcceleration = -(yacceleration);
 
-function createCircle (x, y, r)
+function createCircle (x, y, r, type, color)
 {
 	c = document.getElementById("myCanvas");
 	ctx = c.getContext("2d");
 	ctx.beginPath();
 	ctx.arc(x,y,r, 0 ,2*Math.PI);
-	ctx.stroke();
-	ctx.fillStyle = "black";
-	ctx.fill();
+	if(type == "fill")
+	{
+		ctx.fillStyle = color;
+		ctx.fill();
+	}
+		
+	else
+	{
+		ctx.strokeStyle = color;
+		ctx.stroke();	
+	}
+	
 
 }
 
@@ -18,15 +33,26 @@ function moveCircle()
 
 
 
-	blankIt();
-	if(250 - inc <= 0)
-		createCircle(250, 250, 25);
-	else
-	{
-		createCircle(250, 250 - inc, 25);
-		//change the below constant to alter the force of gravity
-		inc+=(inc*.220);
-	}
+		blankIt();
+			createCircle(500, 250, 50, "fill", "orange");
+		
+		if(ypos < 250)
+		{
+			createCircle(xpos, ypos, 10);
+			yspeed+=yacceleration;
+			ypos+=yspeed;
+			xspeed+=xacceleration;
+			xpos+=xspeed;
+	
+		}
+		else
+		{
+			createCircle(xpos, ypos, 10);
+			yspeed+=negativeYAcceleration;
+			ypos+=yspeed;
+			xspeed+=xacceleration;
+			xpos+=xspeed;
+		}
 	
 
 
@@ -38,7 +64,7 @@ function blankIt()
 	c = document.getElementById("myCanvas");
 	ctx = c.getContext("2d");
 	ctx.fillStyle = "white";
-	ctx.fillRect(0, 0, 500, 500);
-	ctx.clearRect(0, 0, 500, 500);
+	ctx.fillRect(0, 0, 1000, 500);
+	ctx.clearRect(0, 0, 1000, 500);
 
 }
