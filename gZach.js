795b3689d9099bda82937d7411bdfb4e8accf6c9
Zach Teutsch <zteutsch2017@francisparker.org>
2016-12-12 19:54:32 -0800

    Tried to make gravity function on a realistic scale
    5 pixels -> 1 foot
    Converted acceleration due to gravity into pixels per interval squared
    fall time was slightly off, but pretty close after tests

---
+++ gZach.js
@@ -0,0 +1,58 @@
+/*
+Zach Teutsch
+gravity simulation
+JavaScript
+*/
+
+var can = document.getElementById("myCanvas");
+var cont = can.getContext("2d");
+
+// variable to store current downwards velocity due to gravity
+var yVelocity = 15;
+
+var xVelocity = 5;
+
+//acceleration due to gravity in terms of pixel per interval squared
+var accelGrav = -.322;
+
+//radius of falling object
+var objRadius = 10;
+
+//starting points of object
+var currX = 0;
+var currY = 0;
+
+
+
+//regenerate all white background
+
+function redrawBackground(){
+
+  cont.fillStyle = "white";
+  cont.fillRect(0, 0, can.width, can.height);
+
+}
+
+function drawObject(x, y) {
+
+  // cont.beginPath();
+  cont.strokeStyle = "black";
+  cont.arc(x, can.height - y, objRadius ,0,2*Math.PI);
+  cont.stroke();
+  // cont.endPath();
+
+}
+
+function moveObject() {
+
+  redrawBackground();
+
+  currY = currY + yVelocity;
+  yVelocity = yVelocity + accelGrav;
+
+  currX = currX + xVelocity;
+
+  drawObject(currX, currY);
+
+
+}
